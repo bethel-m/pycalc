eb95469a43c304d7ac85ab1fe8dbf3a10e035034
bethel-m <bethelcletus87@gmail.com>
2023-01-19 02:59:25 +0100

    added circleci config

--- calculator.py
+++ calculator.py
@@ -2,8 +2,10 @@
 calculator libary containing basic math operations
 """
 
-def add(first_term,second_term):
-  return first_term + second_term
+
+def add(first_term, second_term):
+    return first_term + second_term
+
 
 def subtract(first_term, second_term):
-  return first_term - second_term
+    return first_term - second_term
